08360d0232fd414b710590375f7f7b99c8f2b1d8
itsmikeramsay <ronaldramsayii@gmail.com>
2014-09-24 21:24:46 -0400

    hlte-common: Move broadcom nfc stuff to individual devices
    Change-Id: Ia6eab06008f78b4012ac0a3a7d262324ee85db8f

--- hlte.mk
+++ hlte.mk
@@ -87,19 +87,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
 # NFC
-ifeq ($(TARGET_NFC_TECH), nxp)
-PRODUCT_PACKAGES += \
-    libnfc \
-    libnfc_jni \
-    Nfc \
-    Tag \
-    com.android.nfc_extras
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
-
-else
-
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     NfcNci \
@@ -107,12 +94,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml \
-    $(LOCAL_PATH)/configs/libnfc-brcm-20791b05.conf:system/etc/libnfc-brcm-20791b05.conf \
-    $(LOCAL_PATH)/configs/libnfc-brcm-20791b04.conf:system/etc/libnfc-brcm-20791b04.conf \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
-
-endif
+    $(LOCAL_PATH)/configs/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
