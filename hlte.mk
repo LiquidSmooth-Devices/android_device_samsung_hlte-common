6c1e4cda44fd27d760bc7738df4587d94a7cef53
Matt Filetto <matt.filetto@gmail.com>
2014-09-28 11:40:14 -0700

    hlte-common: add another missing nfc permission
    Change-Id: I156504696fadde787b3135438d27d2667e82b9f5

--- hlte.mk
+++ hlte.mk
@@ -44,6 +44,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
 
 # Audio
