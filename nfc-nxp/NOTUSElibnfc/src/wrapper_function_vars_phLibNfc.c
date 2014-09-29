743e95c2d11e78dc82c8dcfee47b3c14a9a71982
Matt Filetto <matt.filetto@gmail.com>
2014-09-29 01:05:38 -0700

    hlte-common: remove unused libnfc source
    * This is the exact same source that is in /external/libnfc-nxp
    
    Change-Id: Ie67615ada384c111b94bc94692e65e990da9ba9e

--- nfc-nxp/NOTUSElibnfc/src/wrapper_function_vars_phLibNfc.c
+++
@@ -1,7 +0,0 @@
-/********************** function call vars: phLibNfc **************************/
-
-static NFCSTATUS (*phLibNfc_Download_Mode_) () = NULL;
-
-static NFCSTATUS (*phLibNfc_HW_Reset_) () = NULL;
-
-static int (*phLibNfc_Load_Firmware_Image_) () = NULL;
